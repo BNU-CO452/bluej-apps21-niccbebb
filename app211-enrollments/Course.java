7a5018372e55a48255ed05534a393bd2e7c50eae
niccbebb <nicolecbebb@gmail.com>
2021-12-09 20:19:09 +0000

    Added Module

--- app211-enrollments/Course.java
+++ app211-enrollments/Course.java
@@ -4,16 +4,16 @@ import java.util.*;
  * This class contains information about an undergraduate course
  * at BNU together with a list of student who are enrolled
  *
- * @author Derek Peacock
- * @version 1.0 14/07/2021
+ * @author Nicole Bebb
+ * @version 1.0 12/11/2021
  */
-public class Course
+public class Course extends Module
 {
     // A unique identifier e.g. BT1CTG1
     private String code;
     // The full title including qualification and subject
     private String title;
-    
+    private Module module;
     /**
      * Create a Course with a maximum number of enrolments. 
      * All other details are set to unkown values.
@@ -35,6 +35,8 @@ public class Course
         
         System.out.println(" Course Code: " + code + ": " + title);
         System.out.println();
+        
+        moduleprint();
     }
     
     /**
@@ -46,5 +48,10 @@ public class Course
         System.out.println("   App211: Course Details");
         System.out.println(" --------------------------------");
         System.out.println();
+    }
+    
+    public void addModule(Module module)
+    {
+        this.module = module;
     }
 }
---
+++ app211-enrollments/Module.java
@@ -0,0 +1,33 @@
+
+/**
+ * This class models a BNU module which forms part of a course.
+ *
+ * @author Nicole Bebb
+ * @version 1.0
+ */
+public class Module
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
