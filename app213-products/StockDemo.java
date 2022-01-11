963be7f886d1bb16075b169fdfcdaa0b71f52351
niccbebb <nicolecbebb@gmail.com>
2022-01-11 01:05:44 +0000

    Product created, printed, & bought stock products

--- app213-products/StockDemo.java
+++ app213-products/StockDemo.java
@@ -20,12 +20,16 @@ public class StockDemo
     {
         this.stock = stock;
         
-        // Add at least 10 products, they must be unique to you
-        // Make sure the ids are sequential numbers
-        
-        stock.add(new Product(101, "Samsung Galaxy S20"));
-        stock.add(new Product(102, "Apple iPhone 12"));
-        stock.add(new Product(103, "Google Pixel 4A"));
+        stock.add(new Product(101, "Apple iPhone 3G"));
+        stock.add(new Product(102, "Apple iPhone 4S"));
+        stock.add(new Product(103, "Apple iPhone 5C"));
+        stock.add(new Product(104, "Apple iPhone 6"));
+        stock.add(new Product(105, "Apple iPhone 8"));
+        stock.add(new Product(106, "Apple iPhone XR"));
+        stock.add(new Product(107, "Apple iPhone 11"));
+        stock.add(new Product(108, "Apple iPhone 11 Pro"));
+        stock.add(new Product(109, "Apple iPhone 12"));
+        stock.add(new Product(110, "Apple iPhone 13 Pro Max"));
     }
     
     /**
@@ -50,9 +54,11 @@ public class StockDemo
     
     private void buyProducts()
     {
+        stock.buyProduct(101, 500);
     }
 
     private void sellProducts()
     {
+        
     }    
 }
